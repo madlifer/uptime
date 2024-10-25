51e15cfd1255fd8459f3a64c44a997b3017bb041
madlifer <madlifermail@gmail.com>
2024-10-25 20:01:10 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -8,13 +8,9 @@ const pageConfig = {
     { link: 'mailto:madlifer@live.com', label: '邮件联系我', highlight: true },
   ],
 }
-
 const workerConfig = {
-  // Write KV at most every 3 minutes unless the status changed
   kvWriteCooldownMinutes: 3,
-  // Enable HTTP Basic auth for status page & API by uncommenting the line below, format `<USERNAME>:<PASSWORD>`
-  // passwordProtection: 'username:password',
-  // Define all your monitors here
+  passwordProtection: 'username:password',
   monitors: [
     {
       id: 'website',
@@ -48,20 +44,6 @@ const workerConfig = {
       target: '110.40.78.38:271165',
     },
   ],
-  notification: {
-    // [Optional] apprise API server URL
-    // if not specified, no notification will be sent
-    appriseApiServer: "https://apprise.example.com/notify",
-    // [Optional] recipient URL for apprise, refer to https://github.com/caronc/apprise
-    // if not specified, no notification will be sent
-    recipientUrl: "tgram://bottoken/ChatID",
-    // [Optional] timezone used in notification messages, default to "Etc/GMT"
-    timeZone: "Asia/Shanghai",
-    // [Optional] grace period in minutes before sending a notification
-    // notification will be sent only if the monitor is down for N continuous checks after the initial failure
-    // if not specified, notification will be sent immediately
-    gracePeriod: 5,
-  },
   callbacks: {
     onStatusChange: async (
       env: any,
@@ -69,7 +51,7 @@ const workerConfig = {
       isUp: boolean,
       timeIncidentStart: number,
       timeNow: number,
-      reason: string
+      reason: string,
     ) => {
       // This callback will be called when there's a status change for any monitor
       // Write any Typescript code here
@@ -82,7 +64,7 @@ const workerConfig = {
       monitor: any,
       timeIncidentStart: number,
       timeNow: number,
-      reason: string
+      reason: string,
     ) => {
       // This callback will be called EVERY 1 MINTUE if there's an on-going incident for any monitor
       // Write any Typescript code here
